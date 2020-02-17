44488bd58c22898ec1ff93e2d6163df3e7770ac9
Paulien van Alst <paulien@openvalue.nl>
2020-02-17 08:49:47 +0100

    Initial commit

---
+++ src/main/java/com/barcoding/episode4testing/Episode4TestingApplication.java
@@ -0,0 +1,13 @@
+package com.barcoding.episode4testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Episode4TestingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Episode4TestingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/barcoding/episode4testing/Episode4TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barcoding.episode4testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Episode4TestingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
